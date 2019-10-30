2a8bfd885dee111d2293af1480e093eff9b5a15b
Iweinrazvadovskaya <jane_rob@icloud.com>
2019-10-30 11:16:14 +0300

    Color sectors whith growing on the next lvl

---
+++ colorRings/Assets/Scripts/ArrowController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+
+    public GameObject cameraContainer;
+    public FirstLvl firts = new FirstLvl();
+    List<GameObject> collisionSectors = new List<GameObject>();
+    void Start()
+    {
+       gameObject.SetActive(true);
+    }
+    bool collision = true;
+
+    private void OnTriggerEnter(Collider element)
+    {
+
+        if (element.tag == "SelectSector") { element.gameObject.GetComponent<Renderer>().material.color = new Color(1111, 1111, 1111); }
+            collisionSectors.Add(element.gameObject);
+    }
+
+    void ChangeSectorColor(bool change, GameObject element)
+    {
+        if (change == false&& element.tag=="SelectSector")
+        {
+            element.GetComponent<Renderer>().material.color = new Color(0000, 0000, 0000);
+        }
+    }
+    void BeginPainting()
+    {
+        gameObject.transform.rotation = Quaternion.Euler(1, transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(.8f, 0, 0);
+        if (Input.touchCount > 0 )
+        {
+            BeginPainting();
+                  
+        }
+        else
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, transform.eulerAngles.z);
+            gameObject.transform.position = new Vector3(0, .95f, 0);
+            if (/*collision == true && */collisionSectors.Count!=0)
+            {
+
+                for (int i = 0; i < collisionSectors.Count; i++)
+                {
+                    if (collisionSectors[i].tag !="SelectSector")
+                    {
+                        collision = false;
+                        collisionSectors.Clear();
+                    }
+                    else {
+                        collision = true;
+                    }
+                }
+                if (collision == true)
+                {
+                    for (int i = 0; i < collisionSectors.Count; i++)
+                    {
+                            collisionSectors[i].transform.localScale = new Vector3(0.5555555f, 0.5555555f, 1);
+                      
+                    }
+                    cameraContainer.transform.position = new Vector3(-0.03f, 13, -10.5f);
+                }
+                else
+                {
+                    for (int i = 0; i < collisionSectors.Count; i++)
+                    {
+                       ChangeSectorColor(false,collisionSectors[i]);
+                    }
+                }
+                //collisionSectors[].transform.localScale = new Vector3(transform.localScale.x, 35, transform.localScale.z);
+            }
+        }
+
+    }
+
+}
---
+++ colorRings/Assets/Scripts/FirstLvl.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstLvl : MonoBehaviour
+{
+   
+    List<GameObject> firstLevel = new List<GameObject>();
+    public List<GameObject> sector = new List<GameObject>();
+    public Material redMaterial;
+    int index;
+
+    private double startA;
+    private double endingA;
+
+    void Start()
+    {
+       
+            CreateCollection();
+        index = Random.Range(1, 35);
+        Debug.Log(index);
+        FirstLevelSectors(index);
+    }
+
+    public List<GameObject> Lister()
+    {
+        return sector;
+    }
+void FirstLevelSectors(int i)
+    {
+        Debug.Log(i);
+        for (int j = 0; j < 9; j++) 
+        {
+
+            sector.Add(firstLevel[i]);
+            Debug.Log(sector[j]+"---" + j + "       " + firstLevel[i]);
+            firstLevel[i].GetComponent<Renderer>().material.color = new Color(0000, 0000, 0000); 
+                if (i==35)
+                {
+                    i=0;
+                }
+                else
+                {
+                    i ++;
+                }
+        }
+        AddTagsToSelectSectors();
+    }
+
+    public void AddTagsToSelectSectors()
+    {
+        for(int i = 0; i < sector.Count; i++)
+        {
+            sector[i].tag = "SelectSector";
+        }
+    }
+
+    void CreateCollection()
+    {
+        for (int i = 0; i < 36;)
+        {
+            var sectors = GameObject.Find("littleSector (" + i + ")");
+            //sectors.tag = "NotSelectingSector";
+            firstLevel.Add(sectors);
+            i++;
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
